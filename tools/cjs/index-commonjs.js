342bcee0f323f0513090073b359c6c5503c63d3e
Jos de Jong <wjosdejong@gmail.com>
2020-11-30 11:02:27 +0100

    Remove redundant commonjs index file

--- tools/cjs/index-commonjs.js
+++
@@ -1,7 +0,0 @@
-const immutableJSONPatch = require('./immutableJSONPatch.js').immutableJSONPatch
-const revertJSONPatch = require('./immutableJSONPatch.js').revertJSONPatch
-
-immutableJSONPatch.immutableJSONPatch = immutableJSONPatch
-immutableJSONPatch.revertJSONPatch = revertJSONPatch
-
-module.exports = immutableJSONPatch
